1f37933ad192996373c28e3416b4a1b55c92a664
Alessandro Di Maria <adm@m42.ch>
2025-07-06 14:23:43 +0200

    monsum dark theme, mix setup

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Monsum.MixProject do
       version: "0.1.0",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      # Important for asset compilation
+      compilers: Mix.compilers(),
+      # Include priv directory in package
+      package: package()
     ]
   end
 
@@ -21,8 +25,20 @@ defmodule Monsum.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:phoenix, "~> 1.8.0-rc.3"},
+      {:phoenix_live_view, "~> 1.1.0-rc.1"},
+      {:phoenix_html, "~> 4.0"},
+      {:esbuild, "~> 0.8", runtime: Mix.env() == :dev},
+      {:tailwind, "~> 0.2", runtime: Mix.env() == :dev}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "priv", "assets", "mix.exs", "README.md"],
+      maintainers: ["Alessandro Di Maria"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/grrrisu/monsum"}
     ]
   end
 end
--- test/monsum_test.exs
+++ test/monsum_test.exs
@@ -1,8 +1,4 @@
 defmodule MonsumTest do
   use ExUnit.Case
   doctest Monsum
-
-  test "greets the world" do
-    assert Monsum.hello() == :world
-  end
 end
